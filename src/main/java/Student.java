561479e2eb015a7ac0f5ddcc2e2d58bb1f65e333
recklesskidjo3 <josephs409@gmail.com>
2018-12-15 14:36:12 -0600

    Initial commit

---
+++ src/main/java/Student.java
@@ -0,0 +1,11 @@
+public class Student {
+    String name ;
+
+    public Student(String name) {
+       this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
---
+++ src/test/java/StudentTest.java
@@ -0,0 +1,19 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class StudentTest {
+    @Test
+    public void canCreate() {
+        final String firstStudentName = "Jane Doe"  ;
+        final String SecondStudentName = "Joe Blow ";
+       Student student = new Student(firstStudentName);
+
+       assertEquals(firstStudentName, student.getName());
+
+       Student secondStudent = new Student(SecondStudentName);
+       assertEquals(SecondStudentName, secondStudent.getName());
+
+       assertEquals(firstStudentName, student.getName());
+    }
+}
