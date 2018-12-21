a663390e319738b386f2b67fcc1dc5a96e410bbc
recklesskidjo3 <josephs409@gmail.com>
2018-12-21 00:41:43 -0600

    Working CourseSessionTest

--- src/main/java/CourseSession.java
+++ src/main/java/CourseSession.java
@@ -1,2 +1,20 @@
 public class CourseSession {
+
+    private String department;
+    private String number;
+
+    public CourseSession(String department, String number) {
+        this.department = department;
+        this.number = number;
+
+
+    }
+    String getDepartment(){
+        return department;
+    }
+    String getNumber(){
+        return number;
+    }
+
+
 }
--- src/test/java/CourseSessionTest.java
+++ src/test/java/CourseSessionTest.java
@@ -1,9 +1,14 @@
 import org.junit.Test;
 
+import static junit.framework.TestCase.assertEquals;
+
 public class CourseSessionTest {
     @Test
     public void testCreate() {
-        CourseSession session = new CourseSession(Deprecated, "ENGL");
+
+        CourseSession session = new CourseSession("ENGL", "101");
+        assertEquals("ENGL", session.getDepartment());
+        assertEquals("101", session.getNumber());
 
 
     }
