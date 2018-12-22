4885a24a9bfa5392174429a3772e4f7650302c20
recklesskidjo3 <josephs409@gmail.com>
2018-12-21 23:25:18 -0600

    Combining the test Suites

---
+++ src/test/java/AllTests.java
@@ -0,0 +1,10 @@
+import org.junit.runner.*;
+import org.junit.runners.Suite;
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        StudentTest.class,
+        CourseSessionTest.class,
+})
+
+public class AllTests {
+}
