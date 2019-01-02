1f7bda30d0706cba26d8f10d6123a6c4217a3b3c
recklesskidjo3 <josephs409@gmail.com>
2019-01-02 09:55:28 -0600

    Date Util added to the main repo

---
+++ src/main/java/studentinfo/DateUtil.java
@@ -0,0 +1,17 @@
+package studentinfo;
+import java.util.*;
+
+public class DateUtil {
+    Date createDate(int year, int month, int date){
+        GregorianCalendar calendar = new GregorianCalendar();
+        calendar.clear();
+        calendar.set(calendar.YEAR,year);
+        calendar.set(calendar.MONTH, month -1);
+        calendar.set(calendar.DAY_OF_MONTH, date);
+        calendar.set(Calendar.DAY_OF_MONTH,date);
+        return calendar.getTime();
+
+
+    }
+
+}
